bac783e6dca67c76fee007c6fc59c76bd70a3ea5
Piotr Napierala <piotr.napierala94@gmail.com>
2020-03-18 21:12:47 +0100

    Updates

---
+++ precache-manifest.ba47d8565a268db40b8532de90f4454f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a1a1e73bd26b464d75ce7be085ec6b3",
+    "url": "/React-Monsters-Project/index.html"
+  },
+  {
+    "revision": "9ca741ff928f23f6eff9",
+    "url": "/React-Monsters-Project/static/css/main.1c3f76b7.chunk.css"
+  },
+  {
+    "revision": "a683d3c15696d61584b7",
+    "url": "/React-Monsters-Project/static/js/2.a234ae2c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-Monsters-Project/static/js/2.a234ae2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ca741ff928f23f6eff9",
+    "url": "/React-Monsters-Project/static/js/main.1d40edbf.chunk.js"
+  },
+  {
+    "revision": "6e7bc7ad241f4c74bede",
+    "url": "/React-Monsters-Project/static/js/runtime-main.4c4be92b.js"
+  }
+]);
